3a7ace327dbf0e4ee931bc6d93959a0d45d6ffa2
pavelbesedin <pavelbesedin97@mail.ru>
2023-03-24 22:21:51 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+//    в начале каждой задачи
+//int[ ] arr = generateRandomArray();
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+//        ... // Бухгалтеры попросили посчитать сумму всех выплат за месяц.
+//Напишите программу, которая решит эту задачу, и выведите в консоль результат в формате:
+// «Сумма трат за месяц составила … рублей».
+        int[ ] arr = generateRandomArray();
+
+        int totalMonthSum = 0;
+
+        for (int i : arr) {
+            totalMonthSum += i;
+        }
+        System.out.printf("Сумма трат за месяц составила %d рублей\n", totalMonthSum);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+//        ... // Пишем код для задачи 2
     }
 }
